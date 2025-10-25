3f7c9939fc46cad382e89c3f1e258af744a55e00
Ruslan Salmanov <flainof@gmail.com>
2025-10-25 20:45:10 +0300

    Add '/link' command to bot commands for registration link retrieval

--- src/modules/tg/constants/index.ts
+++ src/modules/tg/constants/index.ts
@@ -2,5 +2,6 @@ import { BotCommand } from "grammy/types";
 
 export const bot_commands: Array<BotCommand> = [
     { command: '/start', description: 'Запуск бота' },
+    { command: '/link', description: 'Получить актуальную ссылку для регистрации' },
     { command: '/help', description: 'Помощь' },
 ];
