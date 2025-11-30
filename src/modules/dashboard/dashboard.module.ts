e7545f2537bbe15100da9f93f8b2e583a64d2d28
Ruslan Salmanov <flainof@gmail.com>
2025-11-30 20:25:16 +0300

    feat: refactor DashboardModule to use TgProvider and improve bot command handling

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,9 @@
 import { Inject, Module, OnModuleInit } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { UserModule } from '../user/user.module';
-import { Bot } from 'grammy';
 import { PROVIDERS } from 'src/shared/constants';
+import { TgProvider } from '../tg/types';
+import { createConversation } from '@grammyjs/conversations';
 
 @Module({
     imports: [UserModule],
@@ -11,15 +12,21 @@ import { PROVIDERS } from 'src/shared/constants';
 })
 export class DashboardModule implements OnModuleInit {
     constructor(
-        @Inject(PROVIDERS.TG_BOT) private readonly tgBot: Bot,
+        @Inject(PROVIDERS.TG_PROVIDER) private readonly tgProvider: TgProvider,
         private readonly dashboardService: DashboardService,
     ) {}
 
-    onModuleInit() {
-        this.tgBot.command('dashboard', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboard.bind(this.dashboardService));
+    private onTgBotInit() {
+        this.tgProvider.bot.use(createConversation(this.dashboardService.onDashboardLinkConversation.bind(this.dashboardService), 'dashboard/link'));
         
-        this.tgBot.callbackQuery('dashboard', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.buildWelcomeScreen.bind(this.dashboardService));
-        this.tgBot.callbackQuery('dashboard/hide', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboardHide.bind(this.dashboardService));
-        this.tgBot.callbackQuery('dashboard/link', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboardLink.bind(this.dashboardService));
+        this.tgProvider.bot.command('dashboard', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboard.bind(this.dashboardService));
+
+        this.tgProvider.bot.callbackQuery('dashboard', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.buildWelcomeScreen.bind(this.dashboardService));
+        this.tgProvider.bot.callbackQuery('dashboard/hide', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboardHide.bind(this.dashboardService));
+        this.tgProvider.bot.callbackQuery('dashboard/link', this.dashboardService.hasAccess.bind(this.dashboardService), this.dashboardService.onDashboardLink.bind(this.dashboardService));
+    }
+
+    onModuleInit() {
+        this.tgProvider.subscribe(this.onTgBotInit.bind(this));
     }
 }
