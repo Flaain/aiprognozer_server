a9053d87e7611eb54fabfd9d1dfdd775cd14b9a2
Ruslan Salmanov <flainof@gmail.com>
2025-12-03 23:24:11 +0300

    feat: enhance ProductService with projection support in getProductById and add findOne method

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ProductRepository } from './product.repository';
 import { PRODUCT_TYPE } from './constants';
-import { Types } from 'mongoose';
+import { AggregateOptions, PipelineStage, ProjectionType, QueryOptions, RootFilterQuery, Types } from 'mongoose';
 import { PRODUCT_SLUGS } from './types';
+import { Product } from './schema/product.schema';
 
 @Injectable()
 export class ProductService {
@@ -12,7 +13,11 @@ export class ProductService {
 
     public getProducts = (userId: Types.ObjectId) => this.productRepository.getProducts(userId);
 
-    public getProductById = (productId: Types.ObjectId | string) => this.productRepository.findById(productId);
+    public getProductById = (productId: Types.ObjectId | string, projection?: ProjectionType<Product>) => this.productRepository.findById(productId, projection);
 
     public getProductBySlug = (slug: PRODUCT_SLUGS) => this.productRepository.findOne({ slug });
+
+    public findOne = (filter?: RootFilterQuery<Product>, projection?: ProjectionType<Product>, options?: QueryOptions<Product>) => this.productRepository.findOne(filter, projection, options);
+
+    public aggregate = <T = Product>(pipeline?: Array<PipelineStage>, options?: AggregateOptions) => this.productRepository.aggregate<T>(pipeline, options);
 }
