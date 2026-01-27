caf213431e804b62e87e2840676222d965468107
Ruslan Salmanov <flainof@gmail.com>
2026-01-27 21:47:02 +0300

    refactor: update TasksDaily schema to include telegram_id and telegram_username properties for enhanced task tracking

--- src/modules/tasks/schema/task.daily.schema.ts
+++ src/modules/tasks/schema/task.daily.schema.ts
@@ -3,6 +3,15 @@ import { TaskBase } from "../types";
 
 @Schema({ timestamps: true, collection: 'tasks_daily' })
 export class TasksDaily implements TaskBase {
+    @Prop({ type: Number, unique: true, index: { unique: true, partialFilterExpression: { id: { $type: 'Number' } } } })
+    telegram_id?: number;
+
+    @Prop({ type: String })
+    telegram_username?: string;
+
+    @Prop({ type: String, required: true })
+    link: string;
+    
     @Prop({ type: String, required: true })
     title: string
 
