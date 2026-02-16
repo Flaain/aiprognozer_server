1846bdc02121c4653c1915d9f02ec5a51e39b557
Ruslan Salmanov <flainof@gmail.com>
2026-02-16 16:56:55 +0300

    refactor: removed observer pattern for bot provider, return but instance instead

--- src/modules/tg/tg.module.ts
+++ src/modules/tg/tg.module.ts
@@ -1,36 +1,23 @@
 import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { TgService } from './tg.service';
-import { Bot } from 'grammy';
-import { TgModuleAsyncOptions } from './types';
+import { Bot, Context } from 'grammy';
+import { TgBot, TgModuleAsyncOptions } from './types';
 import { PROVIDERS } from 'src/shared/constants';
 import { UserModule } from '../user/user.module';
 import { DashboardModule } from '../dashboard/dashboard.module';
-import { UserFromGetMe } from 'grammy/types';
+import { ConversationFlavor } from '@grammyjs/conversations';
 
 @Module({})
 export class TgModule {
     public static async forRootAsync(options: TgModuleAsyncOptions): Promise<DynamicModule> {
-        const provider: Provider = {
-            provide: PROVIDERS.TG_PROVIDER,
+        const provider: Provider<TgBot> = {
+            provide: PROVIDERS.TG_BOT,
             useFactory: async (...deps) => {
                 const { token, config } = await options.useFactory(...deps);
 
                 if (!token) throw new Error('Missing bot token');
 
-                return {
-                    bot: new Bot(token, config),
-                    subscribers: new Set<(botInfo: UserFromGetMe) => void>(),
-                    subscribe: function (subscriber: (botInfo: UserFromGetMe) => void) {
-                        this.subscribers.add(subscriber);
-
-                        return () => {
-                            this.subscribers.delete(subscriber);
-                        };
-                    },
-                    notify: function (botInfo: UserFromGetMe) {
-                        this.subscribers.forEach((subscriber) => subscriber(botInfo));
-                    },
-                }
+                return new Bot<ConversationFlavor<Context>>(token, config);
             },
             inject: options.inject ?? [],
         };
